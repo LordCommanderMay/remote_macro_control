b2aa2c93f20d11707f12b6bcea713a6b37e199de
sean <smay101169@gmail.com>
2022-08-07 08:24:47 -0400

    added sql alchemy declarative base and imports to macro.py

--- macro.py
+++ macro.py
@@ -1,11 +1,30 @@
 import subprocess
-from sqlalchemy import  Column, Integer, String
+from sqlalchemy import Column, Integer, String, Enum
+from base import Base
+import enum
 
-class Macro:
-    def __init__(self, macro_id, name, args: list[str] or None):
-        self.macro_id = macro_id
+
+class MACRO_TYPE(enum.Enum):
+    TERMINAL_COMMAND = 1
+
+
+class Macro(Base):
+    __tablename__ = 'macros'
+    macro_id = Column(Integer, Primary_key=True)
+    name = Column(String)
+    icon = Column(String)
+    macro_type = Column(Enum)
+    command = Column(String)
+
+    def __init__(self, name: str, icon: str, macro_type: enum.Enum, command: str):
         self.name = name
-        self.args = args
+        self.icon = icon
+        self.macro_type = macro_type
+        self.command = command
 
     def exacute(self):
-        process = subprocess.run(self.args, check=True)
+
+        match self.macro_type:
+            case MACRO_TYPE.TERMINAL_COMMAND:
+                process = subprocess.run(self.args, check=True)
+
