44db2564cfea2f6412df10025892fd3ba0ec40ba
sean <smay101169@gmail.com>
2022-08-07 07:59:37 -0400

    added sql alchemy declarative base and imports to macro.py

--- macro.py
+++ macro.py
@@ -1,12 +1,11 @@
 import subprocess
+from sqlalchemy import  Column, Integer, String
 
 class Macro:
-    def __init__(self, macro_id,
-    name,
-    args : list[str] or None):
-	self.macro_id = macro_id
-	self.name = name
-	self.args = args
-	
+    def __init__(self, macro_id, name, args: list[str] or None):
+        self.macro_id = macro_id
+        self.name = name
+        self.args = args
+
     def exacute(self):
-	process = subprocess.run(self.args,check=True)
+        process = subprocess.run(self.args, check=True)
