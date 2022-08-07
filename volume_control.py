551d5766c95bbbf01ad3e051057f7bc3c38ac06a
Sean <smay101169@gmail.com>
2022-08-07 13:12:53 -0400

    worked on volume_control.py

--- volume_control.py
+++ volume_control.py
@@ -2,13 +2,25 @@ import platform
 
 class VolumeController:
 
-    __init__(self):
-    match platform.system():
-        case "Linux":
-            from linux.linux_volume_control import SinkInput
-        case "Darwin":
-            pass
-        case "Windows":
-            pass
-        case _:
-            raise OSError("OS not supported!")
+    def __init__(self):
+        match platform.system():
+            case "Linux":
+                from linux.linux_volume_control import LinuxVolumeController
+                self.OSVolumeController = LinuxVolumeController()
+            case "Darwin":
+                from darwin.darwin_volume_control import DarwinVolumeController
+                self.OSVolumeController = DarwinVolumeController()
+            case "Windows":
+                raise NotImplementedError("Windows support has not been added yet")
+            case _:
+                raise OSError("OS not supported!")
+
+        @property
+        def master_volume(self):
+            return self.OSVolumeController.master_volume
+
+        @property
+        def input_volume(self):
+            return self.OSVolumeController.input_volume
+
+
