b04ed154f6d26fdf92b16c9c72ffd0903bd36e49
sean <smay101169@gmail.com>
2022-08-10 18:45:15 -0400

    update

--- macro.py
+++ macro.py
@@ -1,5 +1,4 @@
-import os
-import subprocess
+import os, subprocess, runpy
 from sqlalchemy import Column, Integer, String, Enum
 import keyboard
 from base import Base
@@ -9,6 +8,7 @@ import enum
 class MacroType(enum.Enum):
     TERMINAL_COMMAND = 1
     KEYBOARD_SHORTCUT = 2
+    PYTHON_SCRIPT = 3
 
 
 class Macro(Base):
@@ -29,12 +29,15 @@ class Macro(Base):
         print(self.macro_type)
 
         match self.macro_type:
+
             case MacroType.TERMINAL_COMMAND:
-
                 os.system(self.args)
-            case MacroType.KEYBOARD_SHORTCUT:
 
+            case MacroType.KEYBOARD_SHORTCUT:
                 keyboard.press_and_release(self.args)
+
+            case MacroType.PYTHON_SCRIPT:
+
 
 
 
