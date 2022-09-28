d7ca02690a4163e6b7cc089925d92acc230461a5
Sean <smay101169@gmail.com>
2022-09-28 14:09:19 -0400

    add create new macro case and add temp bug fix for master_volume_change

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import json
 import os
+import ctypes
 
+import _ctypes
 import zmq
 from volume_control import VolumeController
 from sqlalchemy import create_engine, MetaData
@@ -10,13 +12,14 @@ import socket as Socket
 import macro, socket
 from log import logger
 
-def main():
 
+def main():
     logger.info("Creating/Connect To Database...")
     sql_engine = create_engine("sqlite:///database.db", echo=False, future=True)
     Base.metadata.create_all(bind=sql_engine)
     session = sessionmaker(bind=sql_engine)
     session = session()
+
     logger.info("Success!")
 
     logger.info(f"Starting ZMQ server on {Socket.gethostbyname(Socket.gethostname())}:5566")
@@ -25,27 +28,36 @@ def main():
     socket.bind("tcp://*:5566")
     logger.info("Success!")
 
-
     while True:
         macros = session.query(macro.Macro)
-        for x in macros:
-            print(x.macro_type)
+        # for x in macros:
+            #(x.macro_type)
 
         volume_controller = VolumeController()
-        print(volume_controller.master_volume)
+        #(volume_controller.master_volume)
 
         message = socket.recv()
+        #(message)
         recv_data_packet = json.loads(message.decode("utf-8"))
-        print(recv_data_packet["action"])
+        #(recv_data_packet["action"])
 
         match recv_data_packet["action"]:
             case 'send_data':
-                print(volume_controller.output_muted)
+                #(volume_controller.output_muted)
                 packet = volume_controller.get_volume_data_json()
                 socket.send_string(packet)
 
             case 'change_master_volume':
-                volume_controller.change_master_volume(recv_data_packet['volume'])
+                try:
+                    volume_controller.change_master_volume(recv_data_packet['volume']),
+                except _ctypes.COMError:
+                    pass
+                except OSError:
+                    pass
+
+
+
+
 
             case 'toggle_master_mute':
                 volume_controller.toggle_master_mute()
@@ -58,7 +70,7 @@ def main():
                     if sink_input.app_id == recv_data_packet["sink_id"]:
                         sink_input.change_volume(recv_data_packet["volume"])
 
-            case "toggle_mute_app_mute":
+            case "toggle_mute_app":
                 for sink_input in VolumeController.get_app_volume_controllers():
                     if sink_input.app_id == recv_data_packet["sink_id"]:
                         sink_input.toggle_mute()
@@ -67,6 +79,19 @@ def main():
                 for macro_ in macros:
                     if recv_data_packet["macro_id"] == macro_.macro_id:
                         macro_.execute()
+
+            case "create_macro":
+                new_macro = recv_data_packet["data"]
+                _macro = macro.Macro(
+                    name=new_macro['name'],
+                    desc=new_macro['desc'],
+                    color=new_macro['color'],
+                    icon=new_macro['icon'],
+                    macro_type=new_macro["macro_type"],
+                    args=new_macro['args']
+
+                )
+                session.add(_macro)
 
         session.commit()
         session.close()
